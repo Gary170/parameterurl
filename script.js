e22d9261e365aaf8ab096cbe750344a17243e253
Gary169 <78164004+Gary169@users.noreply.github.com>
2021-04-03 11:00:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ document.getElementById("answer").innerHTML = no1 + no2;
 const urlParams = new URLSearchParams(window.location.search);
 const var1 = urlParams.get('var1');
 const var2 = urlParams.get('var2');
-	}
-	var1 = Number(document.getElementById("number1").value);
-        var2 = Number(document.getElementById("number2").value);
-        document.getElementById("answer").innerHTML = var1 + var2;
-
-};
+document.getElementById("number1").innerHTML = var1;
+document.getElementById("number2").innerHTML = var2;
+document.getElementById("answer").innerHTML = var1 + var2;
