2c722a8b0dd0a6a43fc90aaa77e1daf85c424ca2
Gary169 <78164004+Gary169@users.noreply.github.com>
2021-04-03 10:55:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ var no2 = 0;
 
 function calculate() {
 no1 = Number(document.getElementById("number1").value);
-no2 = Number(document.getElementById("number2").value)
+no2 = Number(document.getElementById("number2").value);
 document.getElementById("answer").innerHTML = no1 + no2;
 }
 
 const urlParams = new URLSearchParams(window.location.search);
 const var1 = urlParams.get('var1');
+const var2 = urlParams.get('var2');
 	}
-	no1 = Number(document.getElementById("number1").value);
-        no2 = Number(document.getElementById("number2").value);
+	var1 = Number(document.getElementById("number1").value);
+        var2 = Number(document.getElementById("number2").value);
+        document.getElementById("answer").innerHTML = var1 + var2;
+
 };
