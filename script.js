d34b1e9d0b8717e34f02832b0bd4146aa7959d4e
Gary169 <78164004+Gary169@users.noreply.github.com>
2021-04-03 10:50:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,9 @@ no2 = Number(document.getElementById("number2").value)
 document.getElementById("answer").innerHTML = no1 + no2;
 }
 
-var getParams = function (url) {
-	var params = {};
-	var parser = document.createElement('a');
-	parser.href = url;
-	var query = parser.search.substring(1);
-	var vars = query.split('&');
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split('=');
-		params[pair[0]] = decodeURIComponent(pair[1]);
+const urlParams = new URLSearchParams(window.location.search);
+const var1 = urlParams.get('var1');
 	}
-	params = Number(document.getElementById("number1").value);
+	no1 = Number(document.getElementById("number1").value);
+        no2 = Number(document.getElementById("number2").value)
 };
